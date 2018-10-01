5cdb6e314e416065b867dbd502988505fb7f4dce
Diogo Cezar <diogo@diogocezar.com>
2018-10-01 11:10:01 -0300

    Lesson 06

---
+++ src/configs/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey            : "AIzaSyDhQt0-__Nt0gkCsDvSOK77BXroqqvb_ag",
+	authDomain        : "dctb-mario-plan.firebaseapp.com",
+	databaseURL       : "https://dctb-mario-plan.firebaseio.com",
+	projectId         : "dctb-mario-plan",
+	storageBucket     : "",
+	messagingSenderId : "501287811246"
+}
---
+++ src/firebase/index.js
@@ -0,0 +1,9 @@
+import firebaseConfig from '../configs/firebaseConfig'
+import firebase       from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+firebase.initializeApp(firebaseConfig)
+firebase.firestore().settings({
+	timestampsInSnapshots: true
+})
